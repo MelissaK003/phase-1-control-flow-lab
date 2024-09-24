96d046ba9cc8bdf0ab7ae9ea4a015051fc16b814
Melissa <melissakiptoo@gmail.com>
2024-09-24 22:56:31 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <=400 && feet >=0)
+  {return ('This one is on me!')}
+  else if (feet <=2000 && feet >= 400)
+  {return('That will be twenty bucks.')}
+  else if (feet <=2500 && feet >= 2000)
+  {return('I will gladly take your thirty bucks.')}
+  else (feet >= 2500)
+    {return('No can do.')}
+  }
+  
+function ternaryCheckCity(City){
+  // Write your code here!
+if (City ==='NYC')
+{return('Ok, sounds good.')}
+else 
+{return('No go.')}
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (Tip === 'generous')
+  {return("Thank you so much.")}
+  else if (Tip === 'not as generous')
+    {return('Thank you.')}
+    else 
+    {return('Bye.')}
 }
